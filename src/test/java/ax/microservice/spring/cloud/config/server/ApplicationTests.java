741666344c7220330a25a837eb3ad6bf0b589786
Vishnu KP <vishnukp588@gmail.com>
2022-03-24 13:14:48 +0530

    microservice-spring-clodu

---
+++ src/test/java/ax/microservice/spring/cloud/config/server/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ax.microservice.spring.cloud.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
